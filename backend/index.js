97f1da94d983ebef3186dc12929414977482b9e8
Shyam Kumar <shyamkumar.s2005@gmail.com>
2026-02-06 15:37:19 +0530

    Allow all CORS origins

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://shyam11.netlify.app'],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
